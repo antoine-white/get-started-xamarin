190d06a7d5319370e67dac401e0ab50d419d6c22
antoine-white <antoine.maxime.bl@gmail.com>
2021-10-27 10:53:36 +0200

    Add project files.

---
+++ App/App/ViewModels/AboutViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Input;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace App.ViewModels
+{
+    public class AboutViewModel : BaseViewModel
+    {
+        private static readonly string TITLE = "About";
+
+        public AboutViewModel()
+        {
+            Title = TITLE;
+            OpenWebCommand = new Command(async () => await Browser.OpenAsync("https://aka.ms/xamarin-quickstart"));
+        }
+
+        public ICommand OpenWebCommand { get; }
+    }
+}
---
+++ App/App/ViewModels/HomeViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Input;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+
+namespace App.ViewModels
+{
+    public class HomeViewModel : BaseViewModel
+    {
+        private const string PROPERTY_NAME = "ButtonStr";
+        private static readonly string TITLE = "Home";
+
+        private int _count;
+
+        public string ButtonStr {private set;get;}
+
+        public HomeViewModel()
+        {
+            _count = 0;
+            Title = TITLE;
+            UpdateButtonStr();
+            AddCount = new Command(() => {
+                _count++;
+                UpdateButtonStr();
+            });
+        }
+
+        private void UpdateButtonStr()
+        {
+            ButtonStr = $"Button has been clicked {_count} times!";
+            OnPropertyChanged(PROPERTY_NAME);
+        }
+
+        public ICommand AddCount { get; }
+    }
+}
